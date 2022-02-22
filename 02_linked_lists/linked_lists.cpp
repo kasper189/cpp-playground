066bb2cc5061213dfd5d12d4ed4ea57347048fa6
Davide Sanapo <davide.sanapo@gmail.com>
2022-02-22 08:54:42 +0100

    ex 2.1

--- 02_linked_lists/linked_lists.cpp
+++ 02_linked_lists/linked_lists.cpp
@@ -1,11 +1,46 @@
 #include <iostream>
-#include <array>
-#include <vector>
 #include <sstream>
+#include <set>
 
 #include "linked_lists.h"
 
+void traverse_and_print(Node* head) {
+    Node* tmp = head;
+    std::cout << "LinkedList: " << std::endl;
+    while(tmp != NULL) {
+        std::cout << "e: " << tmp->data << " | " << std::endl;
+        tmp = tmp->next;
+    }
+}
+
 void run_linked_lists() {
     std::cout << "Hello, Linked Lists!" << std::endl;
+    Node* a = new Node(4, NULL);
+    Node* b = new Node(5, a);
+    Node* c = new Node(1, b);
+    Node* d = new Node(5, c);
     
+    // 2.1
+    delete_dups(d);
+    traverse_and_print(d);
+}
+
+/*2.1*/
+void delete_dups(Node* head) {
+    std::set<int> elements;
+    Node* tmp = head;
+    Node* previous = NULL;
+    
+    while(tmp != NULL) {
+        if(elements.find(tmp->data) != elements.end()) {
+            // element already exists
+            previous->next = tmp->next;
+            delete(tmp);
+            tmp = previous->next;
+        } else{
+            elements.insert(tmp->data);
+            previous = tmp;
+            tmp = tmp->next;
+        }
+    }
 }
--- 02_linked_lists/linked_lists.h
+++ 02_linked_lists/linked_lists.h
@@ -1,7 +1,19 @@
 #ifndef linked_lists_h
 #define linked_lists_h
 
+class Node {
+    public:
+        int data;
+        Node* next;
+        
+    Node(int data, Node* next) : data(data), next(next) {}
+    
+};
+
 void run_linked_lists();
+
+//Exercises
+void delete_dups(Node* head);
 
 
 #endif /* linked_lists_h */
